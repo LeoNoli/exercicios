1ddd5e006e36481da5eda2a3096f9cc4db1163a0
LeoNoli <Nollizleo@gmail.com>
2020-02-19 17:42:00 -0300

    versao b

---
+++ index.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width, initial-sacle=1">
+        <title>Inicio</title>
+    </head>
+    <body>
+        <div class="container">
+            <h1>Barbearia : <a href : https: //exercicios-noli.herokuapp.com></a></h1>
+        </div>
+    </body>
+</html>
